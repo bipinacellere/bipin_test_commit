d4eb6f145b149b30738cfb8ae2b24d446e010460
Bipin Patel <bipin.patel@acellere.com>
2022-02-11 12:32:48 +0530

    FIR-3 new file created

---
+++ index.js
@@ -0,0 +1,17 @@
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+const num3=10;
+
+
+const num4=11;
+
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+var a =5;
+var z=10;
+var x=5;
+var y=10;
